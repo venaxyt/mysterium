1b97ad9023f9130f76d225e43c7c37b60601e0ce
Venax <81310818+venaxyt@users.noreply.github.com>
2021-09-08 15:02:11 +0200

    Update re.py

--- modules/re.py
+++ modules/re.py
@@ -1,70 +1,58 @@
 print("import re")
 
 def findall(pattern, string, flags=0):
-
     print(f"re.findall({pattern}, {string})")
-
+    return ""
 
 def split(pattern, string, maxsplit=0, flags=0):
-
     print(f"re.split({pattern}, {string})")
-
+    return ""
 
 def subn(pattern, repl, string, count=0, flags=0):
-
     print(f"re.subn({pattern}, {repl}, {string})")
-
+    return ""
 
 def sub(pattern, repl, string, count=0, flags=0):
-
     print(f"re.sub({pattern}, {repl}, {string})")
-
+    return ""
 
 def search(pattern, string, flags=0):
-
     print(f"re.search({pattern}, {string})")
-
+    return ""
 
 def fullmatch(pattern, string, flags=0):
-
     print(f"re.fullmatch({pattern}, {string})")
-
+    return ""
 
 def match(pattern, string, flags=0):
-
     print(f"re.match({pattern}, {string})")
-
+    return ""
 
 def finditer(pattern, string, flags=0):
-
     print(f"re.finditer({pattern}, {string})")
-
+    return ""
 
 def purge():
-
     print("re.purge()")
-
+    return ""
 
 def template(pattern, flags=0):
-
     print(f"re.template({pattern})")
-
+    return ""
 
 def escape(pattern):
-
     print(f"re.escape({pattern})")
-
+    return ""
 
 def compile(pattern, flags=0):
-
     pattern  = ""
-
-ASCII        = ""
-IGNORECASE   = ""
-LOCALE       = ""
-UNICODE      = ""
-MULTILINE    = ""
-DOTALL       = ""
-VERBOSE      = ""
-TEMPLATE     = ""
-DEBUG        = ""
+    ASCII        = ""
+    IGNORECASE   = ""
+    LOCALE       = ""
+    UNICODE      = ""
+    MULTILINE    = ""
+    DOTALL       = ""
+    VERBOSE      = ""
+    TEMPLATE     = ""
+    DEBUG        = ""
+    return ""
